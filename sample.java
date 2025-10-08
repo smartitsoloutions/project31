9201576424260ba9914aff47bf34f4b7c6c5d632
smartitsoloutions <prasads128312@gmail.com>
2025-10-08 08:52:23 +0530

    this is my first commit of the project

---
+++ sample.java
@@ -0,0 +1,23 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> textView.setText("Hello, Android Developer!"));
+    }
+}
